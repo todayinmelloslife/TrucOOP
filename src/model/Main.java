ddbd2f3e0003ff238a8d74276e6e2edb1a63c67d
John Doe <mariamello.ads@gmail.com>
2025-03-27 20:01:36 -0300

    ajuste para user adiconar seu nome

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitação do nome dos jogadores
+        System.out.print("Digite o nome do Jogador 1: ");
+        String nomeJogador1 = scanner.nextLine();
+
+        System.out.print("Digite o nome do Jogador 2: ");
+        String nomeJogador2 = scanner.nextLine();
+
         // Criação dos jogadores
-        Jogador jogador1 = new Jogador("Dudinha");
-        Jogador jogador2 = new Jogador("Murilo");
+        Jogador jogador1 = new Jogador(nomeJogador1);
+        Jogador jogador2 = new Jogador(nomeJogador2);
 
         // Criação da interface do jogo
         GameView gameView = new GameView();
@@ -12,5 +23,7 @@ public class Main {
 
         // Início do jogo
         gameController.iniciarJogo();
+
+        scanner.close();
     }
 }
